090799fe9c6c7deabf13105b87907b6182d3ca1a
dj4385 <dj4385@gmail.com>
2019-06-27 15:09:50 +0530

    ui changes

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -72,9 +72,42 @@ import 'core-js/es7/reflect';
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
+//  require('web-animations-js'); 
+//import 'web-animations-js';
 
 
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+// if (!Object.assign) {
+//     Object.defineProperty(Object, 'assign', {
+//       enumerable: false,
+//       configurable: true,
+//       writable: true,
+//       value: function(target, firstSource) {
+//         'use strict';
+//         if (target === undefined || target === null) {
+//           throw new TypeError('Cannot convert first argument to object');
+//         }
+  
+//         var to = Object(target);
+//         for (var i = 1; i < arguments.length; i++) {
+//           var nextSource = arguments[i];
+//           if (nextSource === undefined || nextSource === null) {
+//             continue;
+//           }
+  
+//           var keysArray = Object.keys(Object(nextSource));
+//           for (var nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
+//             var nextKey = keysArray[nextIndex];
+//             var desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
+//             if (desc !== undefined && desc.enumerable) {
+//               to[nextKey] = nextSource[nextKey];
+//             }
+//           }
+//         }
+//         return to;
+//       }
+//     });
+//   }
