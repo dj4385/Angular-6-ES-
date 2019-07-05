00c1798363d7e5186c3ea483ced7d015f6b6bb86
dj4385 <dj4385@gmail.com>
2019-07-05 18:27:03 +0530

    break the cookies string?

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { Router } from '@angular/router';
 import { ClientServiceService } from './client-service.service';
-import { type } from 'os';
 
 
 @Component({
@@ -12,6 +11,12 @@ import { type } from 'os';
 })
 export class CustomerComponent implements OnInit {
   cookieValue = ""
+  cookieArr = []
+  Dbname = ""
+  SuppName = ""
+  
+
+
   @ViewChild('custName') id: ElementRef<HTMLElement>;
 
 
@@ -24,33 +29,50 @@ export class CustomerComponent implements OnInit {
   ngOnInit() {
     this.id.nativeElement.focus();
     this.cookieValue = this._cookieService.get('userInfo')
-    console.log("Cookie value",this.cookieValue)
-    this.checkUserDetails();
-    this.getClientDetails();
+    this.getCookieValues(this.cookieValue)
+    console.log("First Value:",this.cookieArr[0]);
+    
+    
+
+    // console.log("Cookie value",this.cookieValue)
+    // this.checkUserDetails();
+    // this.getClientDetails();
   }
-  
-  checkUserDetails(){
-    if(this.cookieValue === ""){
-      this._route.navigate(['/404']);
+  getCookieValues(cookieData){
+    if(cookieData == ""){
+      this.Dbname = ""
+      this.SuppName = ""
     } else {
-      this._route.navigate(['/customer']);
+      let cookieDataArr = cookieData.split("&",4);
+      let Dname = cookieDataArr[0].toString()
+      let Dbname = Dname.substr(Dname.IndexOf("="), Dname.length);
+      let Sname = cookieDataArr[2].toString()
+      let SuppName = Sname.substr(Sname.IndexOf("="), Sname.length);
+      console.log(Dbname, SuppName);
     }
   }
+  // checkUserDetails(){
+  //   if(this.cookieValue === ""){
+  //     this._route.navigate(['/404']);
+  //   } else {
+  //     this._route.navigate(['/customer']);
+  //   }
+  // }
   
   client = []
   
-  getClientDetails(){
-    this._client.getClient()
-      .subscribe(
-        res => {
-          this.client = res;
-          console.log("Arr:",this.client, typeof(this.client));
-        },
-        err => {
-          console.log(err);
-        }
-      )
-  }
+  // getClientDetails(){
+  //   this._client.getClient()
+  //     .subscribe(
+  //       res => {
+  //         this.client = res;
+  //         console.log("Arr:",this.client, typeof(this.client));
+  //       },
+  //       err => {
+  //         console.log(err);
+  //       }
+  //     )
+  // }
 
   
 
