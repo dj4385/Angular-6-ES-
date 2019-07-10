2538649896f6d01d3e763ee695944a0bda03e36d
dj4385 <dj4385@gmail.com>
2019-07-10 17:24:27 +0530

    add item api integrated

--- src/app/order/item-ser.service.ts
+++ src/app/order/item-ser.service.ts
@@ -7,12 +7,19 @@ import { HttpClient } from '@angular/common/http';
 export class ItemSerService {
 
   itemUrl = "http://localhost:52174/Login.svc/Item";
-
+  itemListUrl = "http://localhost:52174/Login.svc/storeSelectedItems";
+  itemSelectedListUrl = "http://localhost:52174/Login.svc/SelectedItem";
   constructor(
     private _httpClient: HttpClient
   ) { }
 
   getAllItems(){
     return this._httpClient.get(this.itemUrl);
+  }
+  selectItem(code,itemName,userName ,qty, pack,rate,mrp,totalPrice){
+    return this._httpClient.post(this.itemListUrl, {"code":code,"itemName" : itemName,"userName" : userName ,"qty" : qty, "pack" : pack,"rate" : rate,"mrp" : mrp,"totalPrice" : totalPrice} )
+  }
+  getSelectedItemList(){
+    return this._httpClient.get(this.itemSelectedListUrl)
   }
 }
--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -10,19 +10,23 @@ import { ItemSerService } from './item-ser.service';
 export class OrderComponent implements OnInit {
 
   selectedUser = ""
+  totalAmt = ""
   productList:any = []
-  productName: any = []
-  order = {
-    itemName: "",
-    qty:"",
-    balQty:"",
-    pack:"",
-    scheme:"",
-    mrp:"",
-    rate:""
-  }
+  // order :any = {
+    itemName = ""
+    qty = ""
+    balQty = ""
+    pack = ""
+    scheme = ""
+    mrp = ""
+    rate = ""
+  // }
   isClicked = false;
   isReadOnly = true;
+  isQty = false;
+  checkAll = false;
+  selectedItemObj : any = {}
+  selectedItems : any = []
 
   config = {
     displayKey:"name", //if objects array passed which key to be displayed defaults to description
@@ -38,16 +42,13 @@ export class OrderComponent implements OnInit {
   ngOnInit() {
     this.selectedUser = this._activeRoute.snapshot.queryParamMap.get("custName")
     this.getAllProducts();
+    this._getSelectedItemList()
   }
 
   getAllProducts(){
     this._prodService.getAllItems().subscribe(
       res => {
         this.productList = res;
-        this.productList.forEach(element => {
-          this.productName.push(element.name)
-        });
-        console.log(this.productList)
       },
       err => {
         console.log(err);
@@ -55,31 +56,77 @@ export class OrderComponent implements OnInit {
     )
   }
 
-  selectionChanged(data){
+  selectionChanged(data:any){
     this.productList.filter(ele=>{
-      if(data.toLowerCase() == ele.name.toLowerCase()){
-        this.order.balQty = ele.clQty
-        this.order.mrp = ele.mrp
-        this.order.pack = ele.pack
-        this.order.rate = ele.rate
-        this.order.scheme = ele.scheme
+      if(data.value.name.toLowerCase() == ele.name.toLowerCase()){
+        this.code = data.value.code
+        this.itemName = data.value.name
+        this.balQty = ele.clQty
+        this.mrp = ele.mrp
+        this.pack = ele.pack
+        this.rate = ele.rate
+        this.scheme = ele.scheme
       }
     })
   }
 
+  checkQty(qty){
+    if(qty == 0){
+      this.isQty = true
+      this.checkAll = true
+    } else if(parseInt(qty) > parseInt(this.balQty)) {
+      this.isQty = true;
+      this.checkAll = true;
+    } else {
+      this.isQty = false
+      this.checkAll = false
+    }
+    if(this.isQty === false){
+      this.checkAll = false
+    }
+  }
+
+  totalPrice : any = "" 
+  userName = ""
+  code = ""
+
   createOrder(){
-    console.log(this.order);
+    this.totalPrice =parseInt(this.qty) * parseInt(this.rate) 
+    this.totalPrice = String(this.totalPrice)
+    this.userName = this.selectedUser 
+    this.qty = String(this.qty)
+
+    this._prodService.selectItem(this.code,this.itemName,this.userName ,this.qty, this.pack,this.rate,this.mrp,this.totalPrice)
+      .subscribe(
+        res=>{
+          this.reset();
+          this._getSelectedItemList()
+        },
+        err=>{
+          console.log(err)
+        }
+      )
+  }
+  _getSelectedItemList(){
+    this._prodService.getSelectedItemList()
+      .subscribe(
+        res=> {
+          this.selectedItems = res
+          // this.selectedItems = this.selectedItemObj.storeSelectedItemsResult;
+          console.log("Selected Item",this.selectedItems)
+        }
+      )
   }
   reset(){
-    this.order = {
-      itemName: "",
-      qty:"",
-      balQty:"",
-      pack:"",
-      scheme:"",
-      mrp:"",
-      rate:""
-    }
+    // this.order = {
+      this.itemName = ""
+      this.qty = ""
+      this.balQty = ""
+      this.pack = "" 
+      this.scheme = ""
+      this.mrp = ""
+      this.rate = ""
+    // }
   }
   editRow(){
     this.isClicked = true
